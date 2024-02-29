56edfd8d071f13c2305f5622cc3e864c3d5bc408
0x29a <liuhe1009@gmail.com>
2024-02-29 16:09:31 +0800

    w1d3 (#21)

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -18,9 +18,14 @@
 mod builder;
 mod iterator;
 
+use std::mem;
+
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{Buf, BufMut, Bytes};
 pub use iterator::BlockIterator;
+use nom::AsBytes;
+
+const SIZE_OF_U16: usize = mem::size_of::<u16>();
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
 pub struct Block {
@@ -32,11 +37,30 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the course
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut ret = vec![];
+        ret.put(self.data.as_bytes());
+        for o in &self.offsets {
+            ret.put_u16(*o);
+        }
+        ret.put_u16(self.offsets.len() as u16);
+
+        Bytes::from(ret)
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let num_of_elements = (&data[data.len() - SIZE_OF_U16..]).get_u16() as usize;
+        let offset = data.len() - SIZE_OF_U16 - num_of_elements * SIZE_OF_U16;
+
+        let datas = data[0..offset].to_vec();
+        let offsets = data[offset..data.len() - SIZE_OF_U16]
+            .chunks(SIZE_OF_U16)
+            .map(|mut x| x.get_u16())
+            .collect();
+
+        Self {
+            data: datas,
+            offsets,
+        }
     }
 }
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -15,9 +15,11 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
+use bytes::BufMut;
+
 use crate::key::{KeySlice, KeyVec};
 
-use super::Block;
+use super::{Block, SIZE_OF_U16};
 
 /// Builds a block.
 pub struct BlockBuilder {
@@ -34,22 +36,54 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
+    }
+
+    fn current_size(&self) -> usize {
+        SIZE_OF_U16 /*num_of_elements*/ + self.data.len() /* key value pairs*/ + self.offsets.len() *SIZE_OF_U16
+        /*offsets*/
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        // if first data, skip check
+        if !self.is_empty()
+            && self.current_size() + key.len() + value.len() + 3 * SIZE_OF_U16 >= self.block_size
+        {
+            return false;
+        }
+
+        self.offsets.push(self.data.len() as u16);
+
+        self.data.put_u16(key.len() as u16);
+        self.data.put(key.raw_ref());
+
+        self.data.put_u16(value.len() as u16);
+        self.data.put(value);
+
+        if self.first_key.is_empty() {
+            self.first_key = key.to_key_vec();
+        }
+
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.data.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
