53fa6906863b8546edb3def077cf01043fe5b284
ben1009 <liuhe1009@gmail.com>
2024-03-08 17:23:37 +0800

    w1d4

--- mini-lsm-starter/src/table/builder.rs
+++ mini-lsm-starter/src/table/builder.rs
@@ -1,13 +1,18 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use std::path::Path;
 use std::sync::Arc;
+use std::{mem, path::Path};
 
 use anyhow::Result;
+use bytes::BufMut;
 
-use super::{BlockMeta, SsTable};
-use crate::{block::BlockBuilder, key::KeySlice, lsm_storage::BlockCache};
+use super::{BlockMeta, FileObject, SsTable};
+use crate::{
+    block::BlockBuilder,
+    key::{Key, KeySlice},
+    lsm_storage::BlockCache,
+};
 
 /// Builds an SSTable from key-value pairs.
 pub struct SsTableBuilder {
@@ -22,7 +27,14 @@ pub struct SsTableBuilder {
 impl SsTableBuilder {
     /// Create a builder based on target block size.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            builder: BlockBuilder::new(block_size),
+            first_key: Vec::new(),
+            last_key: Vec::new(),
+            data: Vec::new(),
+            meta: Vec::new(),
+            block_size,
+        }
     }
 
     /// Adds a key-value pair to SSTable.
@@ -30,7 +42,28 @@ impl SsTableBuilder {
     /// Note: You should split a new block when the current block is full.(`std::mem::replace` may
     /// be helpful here)
     pub fn add(&mut self, key: KeySlice, value: &[u8]) {
-        unimplemented!()
+        if self.first_key.is_empty() {
+            self.first_key = key.to_key_vec().into_inner();
+        }
+
+        if !self.builder.add(key, value) {
+            let old_builder = mem::replace(&mut self.builder, BlockBuilder::new(self.block_size));
+            let data = old_builder.build().encode();
+
+            let meta = BlockMeta {
+                offset: self.data.len(),
+                first_key: Key::from_vec(self.first_key.clone()).into_key_bytes(),
+                last_key: Key::from_vec(self.last_key.clone()).into_key_bytes(),
+            };
+            self.meta.push(meta);
+
+            self.data.extend(data);
+            self.first_key = key.to_key_vec().into_inner();
+            self.last_key = key.to_key_vec().into_inner();
+            let _ = self.builder.add(key, value);
+        } else {
+            self.last_key = key.to_key_vec().into_inner();
+        }
     }
 
     /// Get the estimated size of the SSTable.
@@ -38,17 +71,43 @@ impl SsTableBuilder {
     /// Since the data blocks contain much more data than meta blocks, just return the size of data
     /// blocks here.
     pub fn estimated_size(&self) -> usize {
-        unimplemented!()
+        self.data.len()
     }
 
     /// Builds the SSTable and writes it to the given path. Use the `FileObject` structure to manipulate the disk objects.
     pub fn build(
-        self,
+        mut self,
         id: usize,
         block_cache: Option<Arc<BlockCache>>,
         path: impl AsRef<Path>,
     ) -> Result<SsTable> {
-        unimplemented!()
+        let meta = BlockMeta {
+            offset: self.data.len(),
+            first_key: Key::from_vec(self.first_key.clone()).into_key_bytes(),
+            last_key: Key::from_vec(self.last_key.clone()).into_key_bytes(),
+        };
+        self.meta.push(meta);
+
+        let data = self.builder.build().encode();
+        self.data.extend(data);
+        let mut buf = self.data;
+        let meta_offset = buf.len();
+
+        BlockMeta::encode_block_meta(&self.meta, &mut buf);
+        buf.put_u32(meta_offset as u32);
+        let file = FileObject::create(path.as_ref(), buf)?;
+
+        Ok(SsTable {
+            file,
+            block_meta: self.meta.clone(),
+            block_meta_offset: meta_offset,
+            id,
+            block_cache,
+            first_key: self.meta[0].first_key.clone(),
+            last_key: self.meta[self.meta.len() - 1].last_key.clone(),
+            bloom: None,
+            max_ts: 0,
+        })
     }
 
     #[cfg(test)]
